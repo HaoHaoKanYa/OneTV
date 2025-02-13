ddcfeb9d3c929e2de72b91afc07bf0cd69a8ad1d
Cywin <cyuan52@qq.com>
2025-02-13 17:18:53 +0800

    2025-2-13完善了一些代码

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
