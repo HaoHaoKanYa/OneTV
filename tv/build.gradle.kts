dab71b9f803e4eee576acda9f676b21243fbd2c5
Cywin <cyuan52@qq.com>
2025-02-20 19:11:11 +0800

    ### 更新了[1.0.6]版本！ -优化 --更新1.0.6版本！ --为优化观看体验，做出下列调整优化： --测试了一些功能。 --测试轮换频道，调试软件稳定性。 --网络线路仅用于调试软件用，使用者请在24小时内删除。 --若您有疑问或建议，欢迎随时联系我们。感谢支持！ --壹来电视仅为空壳软件不提供任何直播源，更多精彩内容请添加自定义直播源。

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0.5"
+        versionName = "1.0.6"
         vectorDrawables {
             useSupportLibrary = true
         }
